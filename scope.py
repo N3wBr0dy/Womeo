47a70c5bd5ba11f9c3f3abc66f7f46a539657cc5
N3wBr0dy <122485846+N3wBr0dy@users.noreply.github.com>
2023-10-02 21:36:50 -0400

    Update_scope.py
    added some fixes for faster screen update and weird orientation

--- scope.py
+++ scope.py
@@ -4,24 +4,25 @@ import picamera.array
 import time
 import numpy as np
 
-# Initialize the Display
+# Initialize the Display-O-Tron HAT Mini
 disp = ST7789.ST7789(
-    width=320,
-    height=240,
     port=0,
     cs=1,
     dc=9,
     backlight=13,
     spi_speed_hz=10000000,
+    width=160,
+    height=120
 )
 disp.begin()
 
 # Initialize the PiCamera
 camera = picamera.PiCamera()
-camera.resolution = (320, 240)
-camera.framerate = 30
+camera.resolution = (160, 120)
+camera.framerate = 60
+camera.rotation = 270  # Rotate the image 90 degrees clockwise
 
-# Start the preview and display it on the Display
+# Start the preview and display it on the Display-O-Tron HAT Mini
 camera.start_preview()
 time.sleep(2) # Wait for the camera to initialize
 try:
@@ -34,10 +35,15 @@ try:
         # Apply the "posterise" effect to the image
         posterised_image = np.array(np.round(image / 64) * 64, dtype=np.uint8)
         
-        # Display the posterised image on the Display
+        # Set the window size and position for the image
+        x_offset = (disp.width - posterised_image.shape[1]) // 2
+        y_offset = (disp.height - posterised_image.shape[0]) // 2
+        disp.set_window(x_offset, y_offset, posterised_image.shape[1], posterised_image.shape[0])
+        
+        # Display the posterised image on the Display-O-Tron HAT Mini
         disp.display(posterised_image)
         
         # Wait for the next frame
-        time.sleep(0.01)
+        time.sleep(0.001)
 finally:
     camera.stop_preview()
