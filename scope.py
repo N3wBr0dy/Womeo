185cfd55b17c5b7eea3b24718d01782bb53edc23
N3wBr0dy <122485846+N3wBr0dy@users.noreply.github.com>
2023-10-02 23:16:29 -0400

    scope4.py

--- scope.py
+++ scope.py
@@ -1,44 +1,69 @@
-import picamera
 import ST7789
-import numpy as np
+import picamera
+import picamera.array
 import time
+import numpy as np
 from PIL import Image
-from PIL import ImageOps
-from io import BytesIO
 
-# Initialize the Display-O-Tron HAT Mini display
-disp = ST7789.ST7789(port=0, cs=1, dc=9, backlight=13, spi_speed_hz=10000000)
+# Initialize the Display-O-Tron HAT Mini
+disp = ST7789.ST7789(
+    port=0,
+    cs=1,
+    dc=9,
+    backlight=13,
+    spi_speed_hz=10000000,
+    width=320,
+    height=240
+)
+disp.begin()
 
-# Set up the camera
+# Initialize the PiCamera
 camera = picamera.PiCamera()
 camera.resolution = (320, 240)
+camera.framerate = 60
+camera.rotation = 270  # Rotate the image 90 degrees clockwise
 
-# Create a function for posterizing an image
-def posterize(image, levels=4):
-    posterized_image = ImageOps.posterize(image, levels)
-    return posterized_image
+# Start the preview and display it on the Display-O-Tron HAT Mini
+camera.start_preview()
+time.sleep(2)  # Wait for the camera to initialize
 
 try:
     while True:
-        # Capture an image from the camera
-        with BytesIO() as stream:
-            camera.capture(stream, format='jpeg')
-            stream.seek(0)
-            image = Image.open(stream)
+        # Capture a frame from the camera as a NumPy array
+        with picamera.array.PiRGBArray(camera) as stream:
+            camera.capture(stream, format='rgb', use_video_port=True)
+            image = stream.array
 
-        # Apply posterize effect
-        posterized_image = posterize(image, levels=4)
+        # Apply the "posterise" effect to the image
+        posterised_image = np.array(np.round(image / 64) * 64, dtype=np.uint8)
 
-        # Resize the posterized image to fit the display
-        posterized_image = posterized_image.resize((240, 240), Image.ANTIALIAS)
+        # Calculate the scaling factors for width and height to fit within the 320x240 display
+        scale_width = 320 / posterised_image.shape[1]
+        scale_height = 240 / posterised_image.shape[0]
 
-        # Convert the posterized image to a numpy array
-        posterized_np = np.array(posterized_image)
+        # Use the minimum of the two scaling factors to maintain aspect ratio
+        scale = min(scale_width, scale_height)
 
-        # Display the posterized image on the Display-O-Tron HAT Mini
-        disp.display(posterized_np)
+        # Resize the image while maintaining its aspect ratio
+        new_width = int(posterised_image.shape[1] * scale)
+        new_height = int(posterised_image.shape[0] * scale)
+        resized_image = Image.fromarray(posterised_image).resize((new_width, new_height))
 
-except KeyboardInterrupt:
-    # Clean up and close the display and camera when Ctrl+C is pressed
-    disp.cleanup()
-    camera.close()
+        # Calculate the position to center the resized image on the 320x240 display
+        x_offset = (320 - new_width) // 2
+        y_offset = (240 - new_height) // 2
+
+        # Convert the resized image back to a NumPy array
+        resized_image = np.array(resized_image, dtype=np.uint8)
+
+        # Set the window size and position for the centered and resized image
+        disp.set_window(x_offset, y_offset, new_width, new_height)
+
+        # Display the centered and resized posterised image on the Display-O-Tron HAT Mini
+        disp.display(resized_image)
+
+        # Wait for the next frame
+        time.sleep(0.001)
+
+finally:
+    camera.stop_preview()
