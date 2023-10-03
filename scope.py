76f5247a3bc1c105943446fd94c8521759aae70a
N3wBr0dy <122485846+N3wBr0dy@users.noreply.github.com>
2023-10-02 23:11:17 -0400

    scope2.py

--- scope.py
+++ scope.py
@@ -1,49 +1,45 @@
-import ST7789
 import picamera
-import picamera.array
-import time
+import ST7789
 import numpy as np
+import cv2
+import time
+from PIL import Image
+from PIL import ImageOps
+from io import BytesIO
 
-# Initialize the Display-O-Tron HAT Mini
-disp = ST7789.ST7789(
-    port=0,
-    cs=1,
-    dc=9,
-    backlight=13,
-    spi_speed_hz=10000000,
-    width=160,
-    height=120
-)
-disp.begin()
+# Initialize the Display-O-Tron HAT Mini display
+disp = ST7789.ST7789(port=0, cs=1, dc=9, backlight=13, spi_speed_hz=10000000)
 
-# Initialize the PiCamera
+# Set up the camera
 camera = picamera.PiCamera()
-camera.resolution = (160, 120)
-camera.framerate = 60
-camera.rotation = 270  # Rotate the image 90 degrees clockwise
+camera.resolution = (320, 240)
 
-# Start the preview and display it on the Display-O-Tron HAT Mini
-camera.start_preview()
-time.sleep(2) # Wait for the camera to initialize
+# Create a function for posterizing an image
+def posterize(image, levels=4):
+    img = ImageOps.posterize(image, levels)
+    return img
+
 try:
     while True:
-        # Capture a frame from the camera as a NumPy array
-        with picamera.array.PiRGBArray(camera) as stream:
-            camera.capture(stream, format='rgb', use_video_port=True)
-            image = stream.array
-            
-        # Apply the "posterise" effect to the image
-        posterised_image = np.array(np.round(image / 64) * 64, dtype=np.uint8)
-        
-        # Set the window size and position for the image
-        x_offset = (disp.width - posterised_image.shape[1]) // 2
-        y_offset = (disp.height - posterised_image.shape[0]) // 2
-        disp.set_window(x_offset, y_offset, posterised_image.shape[1], posterised_image.shape[0])
-        
-        # Display the posterised image on the Display-O-Tron HAT Mini
-        disp.display(posterised_image)
-        
-        # Wait for the next frame
-        time.sleep(0.001)
-finally:
-    camera.stop_preview()
+        # Capture an image from the camera
+        with BytesIO() as stream:
+            camera.capture(stream, format='jpeg')
+            stream.seek(0)
+            image = Image.open(stream)
+
+        # Apply posterize effect
+        posterized_image = posterize(image, levels=4)
+
+        # Convert the posterized image to a numpy array
+        posterized_np = np.array(posterized_image)
+
+        # Resize the numpy array to match the display size (240x240)
+        posterized_np = cv2.resize(posterized_np, (240, 240))
+
+        # Display the posterized image on the Display-O-Tron HAT Mini
+        disp.display(posterized_np)
+
+except KeyboardInterrupt:
+    # Clean up and close the display and camera when Ctrl+C is pressed
+    disp.cleanup()
+    camera.close()
